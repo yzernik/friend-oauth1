955e5d897e6ef8a323483f3530e6bc1e8c8f57e7
yzernik <yzernik@gmail.com>
2015-02-20 23:26:40 -0800

    removed old files

--- src/friend_oauth1/core.clj
+++
@@ -1,6 +0,0 @@
-(ns friend-oauth1.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/friend_oauth1/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns friend-oauth1.core-test
-  (:require [clojure.test :refer :all]
-            [friend-oauth1.core :refer :all]))
-
-(deftest a-test
-  (testing "Fixed"
-    (is (= 0 0))))
